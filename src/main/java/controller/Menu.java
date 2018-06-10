533f62b7def35d5bf45e0253a74bcf7d18c78a05
Tom <Tom@kurs.pl>
2018-06-10 11:52:56 +0200

    dzialajaca aplikacja

--- src/main/java/controller/Menu.java
+++ src/main/java/controller/Menu.java
@@ -1,4 +1,16 @@
 package controller;
 
+import dao.UserDao;
+import model.User;
+
 public class Menu {
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.wyswietlMenu();
+    }
+
+    public void wyswietlMenu(){
+        UserDao userDao= new UserDao();
+        userDao.save(new User("Wojciech", "Musial", "wojciechmusial.com"));
+    }
 }
--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -5,6 +5,7 @@ import confing.Database;
 import model.User;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -17,20 +18,42 @@ public class UserDao {
     private Database database= new Database();
 
     public User user (User user) throws SQLException {
-        // insert
+         // insert
         //1.pobierz polaczenia
         Connection connection = database.getConnection();
-        //2.napisz zapytanie
-        String sql ="INSERT INTO user(first_name, last_name, email) " +"Values()";
+           //2.napisz zapytanie
+          // String sql ="INSERT INTO user(first_name, last_name, email) " +
+         //        "Values(" + user.getFirstName()+ "," +
+        //              user.getLastName()+ "," +
+       //               user.getEmaill()+ ")";
+      //firstName = " Drop datbase"
+        // potencjalnie sql injection
+        String sql ="INSERT INTO user(first_name, last_name, email) " +
+                 "Values (?,?,?,)";
+        //3.Utwórz obiekt PreparedStatement
 
+        PreparedStatement statement = connection.prepareStatement(sql);
+        // Uzupełnij parametry zapytania
+        statement.setString(1,user.getFirstName());
+        statement.setString(2, user.getLastName());
+        statement.setString(3, user.getEmaill());
+
+        //5.wykonaj zapytanie w bazie
+        statement.executeUpdate();
+        return user;
     }
 
     public void update (User user){
 
     }
     public User findById(int id){
-
+        return null;
 
     }
-    public List<User>
+    public List<User> findAll(){
+           return null;
+    }
+
+    public void save(User user) {
+    }
 }
