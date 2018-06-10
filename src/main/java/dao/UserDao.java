02e44347512f82bb7635acf334e239ebd367b104
Tom <Tom@kurs.pl>
2018-06-10 11:23:22 +0200

    User wprowadzane dane

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -1,4 +1,36 @@
 package dao;
 
+
+import confing.Database;
+import model.User;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
 public class UserDao {
+    //save
+    //update
+    //delete
+    //findById
+    //findAll
+    private Database database= new Database();
+
+    public User user (User user) throws SQLException {
+        // insert
+        //1.pobierz polaczenia
+        Connection connection = database.getConnection();
+        //2.napisz zapytanie
+        String sql ="INSERT INTO user(first_name, last_name, email) " +"Values()";
+
+    }
+
+    public void update (User user){
+
+    }
+    public User findById(int id){
+
+
+    }
+    public List<User>
 }
--- src/main/java/model/user.java
+++ src/main/java/model/user.java
@@ -1,5 +1,60 @@
 package model;
 
-public class user {
+public class User {
 
+    //id INT PRIMARY KEY AUTO_INCREMENT,
+   // first_name VARCHAR(100) NOT NULL,
+   // last_name VARCHAR(100) NOT NULL,
+   // email VARCHAR(100) NOT NULL
+
+
+    public   int id;
+    public String firstName;
+    public String lastName;
+    public String emaill;
+
+    public User(int id, String  firstNameame, String lastNameame, String emaill){
+        this.id=id;
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.emaill=emaill;
+    }
+
+    public User(String firstName, String lastName, String emaill) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emaill = emaill;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmaill() {
+        return emaill;
+    }
+
+    public void setEmaill(String emaill) {
+        this.emaill = emaill;
+    }
 }
